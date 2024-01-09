b075e1544aebb78b98e8cf154c8b734fb150c30d
Sigma Pro Max <sgapmx@gmail.com>
2024-01-09 18:42:07 +0800

    initial project

---
+++ src/test/java/com/seeds/mockapi/MockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seeds.mockapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
